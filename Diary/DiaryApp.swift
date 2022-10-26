1ade60873f9a8e0ef68fd97cde69d8ac0ff29900
GNYAR <s901121s901121@gmail.com>
2022-10-26 14:50:23 +0800

    Initial Commit

---
+++ Diary/DiaryApp.swift
@@ -0,0 +1,17 @@
+//
+//  DiaryApp.swift
+//  Diary
+//
+//  Created by User20 on 2022/10/26.
+//
+
+import SwiftUI
+
+@main
+struct DiaryApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
